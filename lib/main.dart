db715201f9e67f4bdf0ca2be77776529d1561c24
cplin <cplin@86links.com>
2020-06-23 00:03:54 +0800

    待添加照片详情操作按钮

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:auto_route/auto_route.dart';
 import 'package:fakensplash/bloc/collection/collection_bloc.dart';
 import 'package:fakensplash/bloc/photo/photo_bloc.dart';
 import 'package:fakensplash/route/splash_router.gr.dart';
-import 'package:fakensplash/ui/page/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -33,6 +32,9 @@ class MyApp extends StatelessWidget {
       child: Wiredash(
         projectId: 'fakensplash-k2gyhjm',
         secret: 'pszdeapx8xfs08u7833odbwi4fd21who',
+        options: WiredashOptionsData(
+          showDebugFloatingEntryPoint: false,
+        ),
         navigatorKey: _navigatorKey,
         child: MaterialApp(
           // navigatorKey: _navigatorKey,
