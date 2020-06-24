62f2e42e1f17e92d4fa73f1da1c5e09b29f52615
cplin <cplin@86links.com>
2020-06-24 23:27:03 +0800

    user profile

--- lib/bloc/user_profile/user_profile_event.dart
+++ lib/bloc/user_profile/user_profile_event.dart
@@ -3,5 +3,5 @@ part of 'user_profile_bloc.dart';
 @immutable
 @freezed
 abstract class UserProfileEvent with _$UserProfileEvent {
-  const factory UserProfileEvent.loadData(String id) = UserProfileLoad;
+  const factory UserProfileEvent.loadData(String username) = UserProfileLoad;
 }
